4778a3cd9d82e29663d40a7960aa427a161d9b23
Taras <smetanyuk94@gmail.com>
2023-12-01 00:39:45 +0200

    7 componeents done
    Without adaptive

--- webstea/src/components/Contact.js
+++ webstea/src/components/Contact.js
@@ -3,9 +3,65 @@ import "../styles/Contact.scss";
 function Contact() {
   return (
     <section className="contact">
-          <p>
-          Contact
-          </p>
+      <div className="container">
+        <h2>Давайте обговоримо вашу ідею!</h2>
+        <div className="contact__inner">
+          <form className="form">
+            <div className="form__row">
+              <input
+                id="name"
+                className="form__input"
+                type="text"
+                placeholder="Ім’я"
+                require
+              ></input>
+              <input
+                id="tel"
+                className="form__input"
+                type="tel"
+                placeholder="Телефон"
+                require
+              ></input>
+            </div>
+            <div className="form__row">
+              <div className="input__inner">
+                <input
+                  id="email"
+                  className="form__input"
+                  type="email"
+                  placeholder="E-mail"
+                  require
+                ></input>
+              </div>
+              <div className="select__inner">
+                <select
+                  placeholder="Тип сайту"
+                  id="type"
+                  className="form__select"
+                  require
+                >
+                  <option selected>Тип сайту</option>
+                  <option value="landing">Landing page</option>
+                  <option value="card">Сайт візитка</option>
+                  <option value="eshop">Інтернет магазин</option>
+                  <option value="corporate">Корпоративний сайт</option>
+                  <option value="catalog">Сайт каталог</option>
+                  <option value="individual">Індивідуальний проект</option>
+                </select>
+              </div>
+            </div>
+
+            <textarea
+              id="message"
+              className="form__textarea"
+              placeholder="Подробиці проєкту"
+            ></textarea>
+            <submit type="submit" className="btn">
+              Відправити
+            </submit>
+          </form>
+        </div>
+      </div>
     </section>
   );
 }
--- webstea/src/components/Stages.js
+++ webstea/src/components/Stages.js
@@ -1,11 +1,138 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 import "../styles/Stages.scss";
 
+import { Pagination } from "swiper/modules";
+
 function Stages() {
+  const stagesPaginationTop = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      const stagesPaginationTopText = [
+        "Аналіз",
+        "Прототип",
+        "Дизайн",
+        "Розробка",
+        "Запуск",
+      ];
+      return (
+        '<span class="' +
+        className +
+        '">' +
+        stagesPaginationTopText[index] +
+        "</span>"
+      );
+    },
+  };
+
   return (
     <section className="stages">
-          <p>
-          Stages
-          </p>
+      <div className="container">
+        <h2>Етапи створення вашого сайту</h2>
+        <div className="stages__inner">
+          <>
+            <Swiper
+              pagination={stagesPaginationTop}
+              modules={[Pagination]}
+              className="stages__slider"
+            >
+              <SwiperSlide>
+                <div className="stages__slide">
+                  <div className="stages__slide-inner">
+                    <div className="stages__slide-text">
+                      <h3>Аналіз</h3>
+                      <p>
+                        Розробляємо оригінальний дизайн, що робить покупки
+                        онлайн зрозумілими і приємними, та який візуально
+                        відтворюватиме фірмовий стиль вашої компанії.
+                        Розробляємо оригінальний дизайн, що робить покупки
+                        онлайн зрозумілими і приємними, та який візуально
+                        відтворюватиме фірмовий стиль вашої компанії.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="stages__slide">
+                  <div className="stages__slide-inner">
+                    <div className="stages__slide-text">
+                      <h3>Прототип</h3>
+                      <p>
+                        Розробляємо оригінальний дизайн, що робить покупки
+                        онлайн зрозумілими і приємними, та який візуально
+                        відтворюватиме фірмовий стиль вашої компанії.
+                        Розробляємо оригінальний дизайн, що робить покупки
+                        онлайн зрозумілими і приємними, та який візуально
+                        відтворюватиме фірмовий стиль вашої компанії.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="stages__slide">
+                  <div className="stages__slide-inner">
+                    <div className="stages__slide-text">
+                      <h3>Дизайн</h3>
+                      <p>
+                        Розробляємо оригінальний дизайн, що робить покупки
+                        онлайн зрозумілими і приємними, та який візуально
+                        відтворюватиме фірмовий стиль вашої компанії.
+                        Розробляємо оригінальний дизайн, що робить покупки
+                        онлайн зрозумілими і приємними, та який візуально
+                        відтворюватиме фірмовий стиль вашої компанії.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="stages__slide">
+                  <div className="stages__slide-inner">
+                    <div className="stages__slide-text">
+                      <h3>Розробка</h3>
+                      <p>
+                        Розробляємо оригінальний дизайн, що робить покупки
+                        онлайн зрозумілими і приємними, та який візуально
+                        відтворюватиме фірмовий стиль вашої компанії.
+                        Розробляємо оригінальний дизайн, що робить покупки
+                        онлайн зрозумілими і приємними, та який візуально
+                        відтворюватиме фірмовий стиль вашої компанії.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="stages__slide">
+                  <div className="stages__slide-inner">
+                    <div className="stages__slide-text">
+                      <h3>Запуск</h3>
+                      <p>
+                        Розробляємо оригінальний дизайн, що робить покупки
+                        онлайн зрозумілими і приємними, та який візуально
+                        відтворюватиме фірмовий стиль вашої компанії.
+                        Розробляємо оригінальний дизайн, що робить покупки
+                        онлайн зрозумілими і приємними, та який візуально
+                        відтворюватиме фірмовий стиль вашої компанії.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </>
+          <div className="pagination__bottom">
+            <span className="pagination__bottom-item"></span>
+            <span className="pagination__bottom-item"></span>
+            <span className="pagination__bottom-item"></span>
+            <span className="pagination__bottom-item"></span>
+            <span className="pagination__bottom-item"></span>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
