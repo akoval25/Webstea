35da41b02767266c48b7a601d50af934c4b97f14
Taras <smetanyuk94@gmail.com>
2023-12-02 00:13:54 +0200

    Fully completed desktop
    Congrats))))))

--- webstea/src/components/FAQ.js
+++ webstea/src/components/FAQ.js
@@ -1,11 +1,78 @@
+import { useState, useRef } from "react";
 import "../styles/FAQ.scss";
 
 function FAQ() {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const [contentHeight, setContentHeight] = useState(0);
+  const contentRef = useRef(null);
+
+  const handleAccordionClick = (index) => {
+    setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
+    if (contentRef.current && activeIndex === index) {
+      setContentHeight(0);
+    } else if (contentRef.current) {
+      setContentHeight(contentRef.current.scrollHeight);
+    }
+  };
+
+  const accordionItems = [
+    {
+      title: "Перше запитання якесь дуже важливе?",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Друге запитання якесь дуже важливе?",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Третє запитання якесь дуже важливе?",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "Четверте запитання якесь дуже важливе?",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      title: "П'яте запитання якесь дуже важливе?",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+  ];
+
   return (
     <section className="faq">
-          <p>
-          FAQ
-          </p>
+      <div className="container">
+        <h2 className="text-right">Часті запитання</h2>
+        <div className="faq__inner">
+          <div className="accordion__inner">
+            {accordionItems.map((item, index) => (
+              <div className="accordion__item" key={index}>
+                <h3
+                  className="accordion__item-title"
+                  onClick={() => handleAccordionClick(index)}
+                >
+                  {item.title}
+                </h3>
+                <div
+                  className={`accordion__item-content ${
+                    activeIndex === index ? "active" : ""
+                  }`}
+                  style={{
+                    maxHeight: activeIndex === index ? contentHeight : 0,
+                  }}
+                  ref={contentRef}
+                >
+                  <p>{item.content}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- webstea/src/components/Footer.js
+++ webstea/src/components/Footer.js
@@ -1,12 +1,76 @@
-import "../styles/Footer.css";
+import "../styles/Footer.scss";
+import logo from "../images/logo.svg";
+import Telegram from "../images/Telegram.svg";
+import Linkedin from "../images/Linkedin.svg";
+import Instagram from "../images/Instagram.svg";
+import Facebook from "../images/Facebook.svg";
+import Behance from "../images/Behance.svg";
 
 function Footer() {
   return (
-    <div className="footer">
-      <footer>
-        <p>footer</p>
-      </footer>
-    </div>
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__inner">
+          <a href="#" className="logo__link">
+            <img src={logo}></img>
+          </a>
+          <ul className="footer__list">
+            <li className="footer__item">
+              <a href="#" className="footer__link">
+                Про нас
+              </a>
+            </li>
+            <li className="footer__item">
+              <a href="#" className="footer__link">
+                Послуги
+              </a>
+            </li>
+            <li className="footer__item">
+              <a href="#" className="footer__link">
+                Портфоліо
+              </a>
+            </li>
+            <li className="footer__item">
+              <a href="#" className="footer__link">
+                Блог
+              </a>
+            </li>
+            <li className="footer__item">
+              <a href="#" className="footer__link">
+                FAQ
+              </a>
+            </li>
+          </ul>
+          <div className="footer__contact">
+            <div className="footer__contact-item">
+              <p>E-mail:</p>
+              <a href="#">webstea@webstea.com</a>
+            </div>
+            <div className="footer__contact-item">
+              <p>Телефон:</p>
+              <a href="#">+3809333333333</a>
+            </div>
+            <div className="footer__contact-social">
+              <a href="#">
+                <img src={Telegram} alt="Telegram"></img>
+              </a>
+              <a href="#">
+                <img src={Linkedin} alt="Linkedin"></img>
+              </a>
+              <a href="#">
+                <img src={Instagram} alt="Instagram"></img>
+              </a>
+              <a href="#">
+                <img src={Facebook} alt="Facebook"></img>
+              </a>
+              <a href="#">
+                <img src={Behance} alt="Behance"></img>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
 
