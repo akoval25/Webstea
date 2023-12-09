304301d5257478f2634d2bffaed54e9f0346cc86
akoval25 <88685936+akoval25@users.noreply.github.com>
2023-12-09 17:30:25 +0200

    last fix
    last fix

--- webstea/src/index.js
+++ webstea/src/index.js
@@ -7,30 +7,49 @@ import reportWebVitals from "./reportWebVitals";
 import { LangProvider } from './scripts/LangContext.js';
 import App from "./components/App.js";
 
+
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/Webstea" element={<App />}>
-      <Route path="/Webstea/en" element={<App />} />
-      <Route path="/Webstea/ua" element={<App />} />
-    </Route>
-  )
-);
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const router = createHashRouter([
+  {
+    path: "/en",
+    element: <App />,
+  },
+  {
+    path: "/ua",
+    element: <App />,
+  },
+  {
+    path: "/Webstea",
+    element: <App />,
+  }, 
+  {
+    path: "/Webstea/en",
+    element: <App />,
+  },
+  {
+    path: "/Webstea/ua",
+    element: <App />,
+  },
+  {
+    path: "/",
+    element: <App />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LangProvider>
     <RouterProvider router={router} />
     </LangProvider>
   </React.StrictMode>
 );
+
+
 
 
 
@@ -63,5 +82,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 //   // </React.StrictMode>
 // );
 
-// reportWebVitals();
+reportWebVitals();
 
