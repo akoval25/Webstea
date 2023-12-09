c33e757f35b0a7dac8ecf2103b5daf7c44b7762a
akoval25 <88685936+akoval25@users.noreply.github.com>
2023-12-09 16:29:53 +0200

    mfk
    mfk

--- webstea/src/index.js
+++ webstea/src/index.js
@@ -1,26 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
 import { LangProvider } from './scripts/LangContext.js';
 import App from "./components/App.js";
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/Webstea/en" element={<App />} />
+      <Route path="/Webstea/ua" element={<App />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 <React.StrictMode>
   <LangProvider>
+  
     <App/>
+
+    <RouterProvider router={router} />
     {/* <Router>
       <Routes>
         <Route path="/Webstea/en" element={<App />} />
         <Route path="/Webstea/ua" element={<App />} />
       </Routes>
-    </Router> */}
+      </Router>   */}
   </LangProvider>
   </React.StrictMode>
   // <React.StrictMode>
