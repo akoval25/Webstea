192b5dfb493b733d17128e5b81d6dc840b235ad6
Taras <smetanyuk94@gmail.com>
2024-01-27 23:36:00 +0200

    404 and Blog page
    done

--- webstea/src/components/Articles.js
+++ webstea/src/components/Articles.js
@@ -1,13 +1,120 @@
 import "../styles/Articles.scss";
-
+import articleImg1 from "../images/article-item.png";
 
 function Articles() {
-
   return (
-    <section className="articles">
-
-          <h2> Самі цікаві статті!</h2>
-  
+    <section className="articles section__mb section__first">
+      <div className="article__inner">
+        <div className="container">
+          <a href="/" className="home-link">
+            На головну
+          </a>
+          <div className="article__items">
+            <div className="article__item">
+              <div className="article__img">
+                <img alt="article item" src={articleImg1}></img>
+              </div>
+              <h3 className="article__title">
+                Штучний інтелект для тестувальника. Загроза чи помічник?
+              </h3>
+              <p className="article__description">
+                Штучний інтелект для тестувальника. Загроза чи помічник? Штучний
+                інтелект для тестувальника. Загроза чи помічник?{" "}
+              </p>
+              <div className="text-right">
+                <a href="#" className="btn">
+                  Дивитись
+                </a>
+              </div>
+            </div>
+            <div className="article__item">
+              <div className="article__img">
+                <img alt="article item" src={articleImg1}></img>
+              </div>
+              <h3 className="article__title">
+                Штучний інтелект для тестувальника. Загроза чи помічник?
+              </h3>
+              <p className="article__description">
+                Штучний інтелект для тестувальника. Загроза чи помічник? Штучний
+                інтелект для тестувальника. Загроза чи помічник?{" "}
+              </p>
+              <div className="text-right">
+                <a href="#" className="btn">
+                  Дивитись
+                </a>
+              </div>
+            </div>
+            <div className="article__item">
+              <div className="article__img">
+                <img alt="article item" src={articleImg1}></img>
+              </div>
+              <h3 className="article__title">
+                Штучний інтелект для тестувальника. Загроза чи помічник?
+              </h3>
+              <p className="article__description">
+                Штучний інтелект для тестувальника. Загроза чи помічник? Штучний
+                інтелект для тестувальника. Загроза чи помічник?{" "}
+              </p>
+              <div className="text-right">
+                <a href="#" className="btn">
+                  Дивитись
+                </a>
+              </div>
+            </div>
+            <div className="article__item">
+              <div className="article__img">
+                <img alt="article item" src={articleImg1}></img>
+              </div>
+              <h3 className="article__title">
+                Штучний інтелект для тестувальника. Загроза чи помічник?
+              </h3>
+              <p className="article__description">
+                Штучний інтелект для тестувальника. Загроза чи помічник? Штучний
+                інтелект для тестувальника. Загроза чи помічник?{" "}
+              </p>
+              <div className="text-right">
+                <a href="#" className="btn">
+                  Дивитись
+                </a>
+              </div>
+            </div>
+            <div className="article__item">
+              <div className="article__img">
+                <img alt="article item" src={articleImg1}></img>
+              </div>
+              <h3 className="article__title">
+                Штучний інтелект для тестувальника. Загроза чи помічник?
+              </h3>
+              <p className="article__description">
+                Штучний інтелект для тестувальника. Загроза чи помічник? Штучний
+                інтелект для тестувальника. Загроза чи помічник?{" "}
+              </p>
+              <div className="text-right">
+                <a href="#" className="btn">
+                  Дивитись
+                </a>
+              </div>
+            </div>
+            <div className="article__item">
+              <div className="article__img">
+                <img alt="article item" src={articleImg1}></img>
+              </div>
+              <h3 className="article__title">
+                Штучний інтелект для тестувальника. Загроза чи помічник?
+              </h3>
+              <p className="article__description">
+                Штучний інтелект для тестувальника. Загроза чи помічник? Штучний
+                інтелект для тестувальника. Загроза чи помічник?{" "}
+              </p>
+              <div className="text-right">
+                <a href="#" className="btn">
+                  Дивитись
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- webstea/src/components/NotFound.js
+++ webstea/src/components/NotFound.js
@@ -1,13 +1,17 @@
 import "../styles/NotFound.scss";
 
-
 function NotFound() {
-
   return (
-    <section className="notfound">
-
-          <h2> 404</h2>
-  
+    <section className="notfound section__mb section__first">
+      <div className="notfound__inner">
+        <div className="container">
+          <h2 className="text-404"> 404</h2>
+          <div className="notfound__bottom">
+            <p>Упс! Сторінка не знайдена.</p>
+            <a className="btn">Назад</a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
