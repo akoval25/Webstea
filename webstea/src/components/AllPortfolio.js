05b2f1385ba16b86c2250184f58f8cf059f61a72
Taras <smetanyuk94@gmail.com>
2024-03-28 00:56:59 +0200

    Update AllPortfolio.js
    Update allportfolio page

--- webstea/src/components/AllPortfolio.js
+++ webstea/src/components/AllPortfolio.js
@@ -1,17 +1,92 @@
 
 import "../styles/AllPortfolio.scss";
-
+import articleImg1 from "../images/article-item.png";
 
 function AllPortfolio() {
 
   return (
-    <section className="allportfolio">
-
-          <div className="allportfolio__top">
-
+    <section className="articles section__mb section__first">
+      <div className="article__inner">
+        <div className="container">
+          <a href="/" className="home-link">
+            На головну
+          </a>
+          <div className="article__items">
+            <div className="article__item">
+              <div className="article__img">
+                <img alt="article item" src={articleImg1}></img>
+              </div>
+              <h3 className="article__title">
+                Штучний інтелект для тестувальника. Загроза чи помічник?
+              </h3>
+              <p className="article__description">
+                Штучний інтелект для тестувальника. Загроза чи помічник? Штучний
+                інтелект для тестувальника. Загроза чи помічник?{" "}
+              </p>
+            </div>
+            <div className="article__item">
+              <div className="article__img">
+                <img alt="article item" src={articleImg1}></img>
+              </div>
+              <h3 className="article__title">
+                Штучний інтелект для тестувальника. Загроза чи помічник?
+              </h3>
+              <p className="article__description">
+                Штучний інтелект для тестувальника. Загроза чи помічник? Штучний
+                інтелект для тестувальника. Загроза чи помічник?{" "}
+              </p>
+            </div>
+            <div className="article__item">
+              <div className="article__img">
+                <img alt="article item" src={articleImg1}></img>
+              </div>
+              <h3 className="article__title">
+                Штучний інтелект для тестувальника. Загроза чи помічник?
+              </h3>
+              <p className="article__description">
+                Штучний інтелект для тестувальника. Загроза чи помічник? Штучний
+                інтелект для тестувальника. Загроза чи помічник?{" "}
+              </p>
+            </div>
+            <div className="article__item">
+              <div className="article__img">
+                <img alt="article item" src={articleImg1}></img>
+              </div>
+              <h3 className="article__title">
+                Штучний інтелект для тестувальника. Загроза чи помічник?
+              </h3>
+              <p className="article__description">
+                Штучний інтелект для тестувальника. Загроза чи помічник? Штучний
+                інтелект для тестувальника. Загроза чи помічник?{" "}
+              </p>
+            </div>
+            <div className="article__item">
+              <div className="article__img">
+                <img alt="article item" src={articleImg1}></img>
+              </div>
+              <h3 className="article__title">
+                Штучний інтелект для тестувальника. Загроза чи помічник?
+              </h3>
+              <p className="article__description">
+                Штучний інтелект для тестувальника. Загроза чи помічник? Штучний
+                інтелект для тестувальника. Загроза чи помічник?{" "}
+              </p>
+            </div>
+            <div className="article__item">
+              <div className="article__img">
+                <img alt="article item" src={articleImg1}></img>
+              </div>
+              <h3 className="article__title">
+                Штучний інтелект для тестувальника. Загроза чи помічник?
+              </h3>
+              <p className="article__description">
+                Штучний інтелект для тестувальника. Загроза чи помічник? Штучний
+                інтелект для тестувальника. Загроза чи помічник?{" "}
+              </p>
+            </div>
           </div>
-          <div className="allportfolio__items">test</div>
-  
+        </div>
+      </div>
     </section>
   );
 }
