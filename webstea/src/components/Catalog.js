fdd7822205fb9abd91c1be1b38f9525d2161d3e7
akoval25 <88685936+akoval25@users.noreply.github.com>
2024-06-08 13:43:44 +0300

    services lang
    services lang

--- webstea/src/components/Catalog.js
+++ webstea/src/components/Catalog.js
@@ -1,54 +1,55 @@
 import "../styles/Catalog.scss";
 import landingImg from "../images/landing1x1.png";
+import { useLang } from "../scripts/LangContext.js";
 
 function Catalog() {
-
+  const { getLangText } = useLang();
   return (
     <section className="landing section__mb section__first">
       <div className="container">
         <div className="section__mb">
-          <h1 className="text-center">Сайт Каталог</h1>
-          <h2 className="text-center">
-          Відмінна альтернатива фізичним каталогам.
+          <h1 className="lng-catalog-title text-center">{getLangText("catalog-title")}</h1>
+          <h2 className="lng-catalog-description text-center">
+          {getLangText("catalog-description")}
           </h2>
         </div>
         <div className="inner__mb component__text-image">
           <div className="content-text">
-            <h3 className="desktop-visible">Що це?</h3>
-            <p className="inner__mb">
-            Сайт Каталог в еру цифрових технологій став лідером завдяки низькій собівартості. Адже, тепер не потрібно друкувати величезний тираж і при оновленні асортименту дозамовляти додаткові примірники. Онлайн-версія каталогу зручна тим, що може просуватися інтернет-простором як через соціальні мережі, особисті повідомлення в мессенджерах, електронне листування так і платними способами, наприклад, контекстна реклама. Для подібного сайту легко відтворити структуру паперового попередника і додати безліч рішень для комфортного використання відвідувачами.</p>   
+            <h3 className="lng-catalog-first-bold desktop-visible">{getLangText("catalog-first-bold")}</h3>
+            <p className="lng-catalog-first-description inner__mb">
+            {getLangText("catalog-first-description")}</p>   
           </div>
           <div className="content-img">
-            <h3 className="mobile-visible text-center">Що це?</h3>
+            <h3 className="lng-catalog-first-bold mobile-visible text-center">{getLangText("catalog-first-bold")}</h3>
             <img alt="landing" src={landingImg}></img>
           </div>
         </div>
         <div className="text-center inner__mb">
-          <a className="btn">Замовити</a>
+          <a className="lng-catalog-order btn" href="#contact">{getLangText("catalog-order")}</a>
         </div>
         <div className="inner__mb component__list">
-          <h3>Переваги Сайту Каталогу</h3>
+          <h3 className="lng-catalog-second-bold">{getLangText("catalog-second-bold")}</h3>
           <ul className="list-plus inner__mb">
-            <li>Низька собівартість;</li>
-            <li>Більше можливостей для просування онлайн;</li>
-            <li>Ефективний брендинг для чіткої ідентифікації;</li>
-            <li>Зручна навігація;</li>
-            <li>Ідеальна організація категорій та фільтрів.</li>
+            <li className="lng-catalog-second-a-description">{getLangText("catalog-second-a-description")}</li>
+            <li className="lng-catalog-second-b-description">{getLangText("catalog-second-b-description")}</li>
+            <li className="lng-catalog-second-c-description">{getLangText("catalog-second-c-description")}</li>
+            <li className="lng-catalog-second-d-description">{getLangText("catalog-second-d-description")}</li>
+            <li className="lng-catalog-second-e-description">{getLangText("catalog-second-e-description")}</li>
 
 
           </ul>
         </div>
         <div className="section__mb component__list">
-          <h3>Умови роботи</h3>
+          <h3 className="lng-catalog-third-bold">{getLangText("catalog-third-bold")}</h3>
           <ul className="list-circle">
-            <li>Консультація</li>
-            <li>Визначення вартості</li>
-            <li>Складання договору та його підписання</li>
-            <li>Створення та погодження технічного завдання</li>
-            <li>Поетапна розробка з демонстрацією проміжного результату</li>
-            <li>Тестування</li>
-            <li>Оплата по факту виконання</li>
-            <li>Підтримка та допомога з наповненням (за бажанням замовника)</li>
+            <li className="lng-catalog-third-a-description">{getLangText("catalog-third-a-description")}</li>
+            <li className="lng-catalog-third-b-description">{getLangText("catalog-third-b-description")}</li>
+            <li className="lng-catalog-third-c-description">{getLangText("catalog-third-c-description")}</li>
+            <li className="lng-catalog-third-d-description">{getLangText("catalog-third-d-description")}</li>
+            <li className="lng-catalog-third-e-description">{getLangText("catalog-third-e-description")}</li>
+            <li className="lng-catalog-third-f-description">{getLangText("catalog-third-f-description")}</li>
+            <li className="lng-catalog-third-g-description">{getLangText("catalog-third-g-description")}</li>
+            <li className="lng-catalog-third-h-description">{getLangText("catalog-third-h-description")}</li>
           </ul>
         </div>
       </div>
--- webstea/src/components/Custom.js
+++ webstea/src/components/Custom.js
@@ -1,51 +1,52 @@
 import "../styles/Custom.scss";
 import landingImg from "../images/landing1x1.png";
+import { useLang } from "../scripts/LangContext.js";
 
 function Custom() {
-
+  const { getLangText } = useLang();
   return (
     <section className="landing section__mb section__first">
       <div className="container">
         <div className="section__mb">
-          <h1 className="text-center">Індивідуальний проект</h1>
-          <h2 className="text-center">
-          Повністю унікальна розробка з урахуванням всіх особливостей бізнесу.
+          <h1 className="lng-custom-title text-center">{getLangText("custom-title")}</h1>
+          <h2 className="lng-custom-description text-center">
+          {getLangText("custom-description")}
           </h2>
         </div>
         <div className="inner__mb component__text-image">
           <div className="content-text">
-            <h3 className="desktop-visible">Що це?</h3>
-            <p className="inner__mb">
-            Індивідуальний сайт під ключ дозволить реалізувати сміливі рішення та приємно здивувати його відвідувачів. Під час створення дизайну ми ретельно дослідимо всі побажання і тонкощі проекту, щоб результат перевершив можливі очікування.</p>   
+            <h3 className="lng-custom-first-bold desktop-visible">{getLangText("custom-first-bold")}</h3>
+            <p className="lng-custom-first-description inner__mb">
+            {getLangText("custom-first-description")}</p>   
           </div>
           <div className="content-img">
-            <h3 className="mobile-visible text-center">Що це?</h3>
+            <h3 className="lng-custom-first-bold mobile-visible text-center">{getLangText("custom-first-bold")}</h3>
             <img alt="landing" src={landingImg}></img>
           </div>
         </div>
         <div className="text-center inner__mb">
-          <a className="btn">Замовити</a>
+          <a className="lng-custom-order btn" href="#contact">{getLangText("custom-order")}</a>
         </div>
         <div className="inner__mb component__list">
-          <h3>Переваги Індивідуального проекту</h3>
+          <h3 className="lng-custom-second-bold">{getLangText("custom-second-bold")}</h3>
           <ul className="list-plus inner__mb">
-            <li>Унікальний дизайн;</li>
-            <li>Особливий функціонал;</li>
-            <li>Зручна навігація;</li>
-            <li>Кросбраузерність.</li>
+            <li className="lng-custom-second-a-description">{getLangText("custom-second-a-description")}</li>
+            <li className="lng-custom-second-b-description">{getLangText("custom-second-b-description")}</li>
+            <li className="lng-custom-second-c-description">{getLangText("custom-second-c-description")}</li>
+            <li className="lng-custom-second-d-description">{getLangText("custom-second-d-description")}</li>
           </ul>
         </div>
         <div className="section__mb component__list">
-          <h3>Умови роботи</h3>
+          <h3 className="lng-custom-third-bold">{getLangText("custom-third-bold")}</h3>
           <ul className="list-circle">
-            <li>Консультація</li>
-            <li>Визначення вартості</li>
-            <li>Складання договору та його підписання</li>
-            <li>Створення та погодження технічного завдання</li>
-            <li>Поетапна розробка з демонстрацією проміжного результату</li>
-            <li>Тестування</li>
-            <li>Оплата по факту виконання</li>
-            <li>Підтримка та допомога з наповненням (за бажанням замовника)</li>
+            <li className="lng-custom-third-a-description">{getLangText("custom-third-a-description")}</li>
+            <li className="lng-custom-third-b-description">{getLangText("custom-third-b-description")}</li>
+            <li className="lng-custom-third-c-description">{getLangText("custom-third-c-description")}</li>
+            <li className="lng-custom-third-d-description">{getLangText("custom-third-d-description")}</li>
+            <li className="lng-custom-third-e-description">{getLangText("custom-third-e-description")}</li>
+            <li className="lng-custom-third-f-description">{getLangText("custom-third-f-description")}</li>
+            <li className="lng-custom-third-g-description">{getLangText("custom-third-g-description")}</li>
+            <li className="lng-custom-third-h-description">{getLangText("custom-third-h-description")}</li>
           </ul>
         </div>
       </div>
